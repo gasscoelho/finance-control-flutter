a5d84a53679c520464c90e70d84c70c74d252f80
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2019-09-26 01:25:51 -0300

    Color changes

--- lib/widgets/drawer_menu.dart
+++ lib/widgets/drawer_menu.dart
@@ -1,3 +1,4 @@
+import 'package:finance_control/util/colors_arsenal.dart';
 import 'package:flutter/material.dart';
 
 class DrawerMenu extends StatelessWidget {
@@ -12,7 +13,10 @@ class DrawerMenu extends StatelessWidget {
           gradient: LinearGradient(
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
-              colors: <Color>[Color(0xff4285F4), Color(0xff9fc1f9)])),
+              colors: <Color>[
+            ColorsArsenal.backgroundColorLight,
+            ColorsArsenal.backgroundColorLight
+          ])),
     );
 
     return Drawer(
@@ -24,35 +28,64 @@ class DrawerMenu extends StatelessWidget {
             children: <Widget>[
               Container(
                 child: UserAccountsDrawerHeader(
+                  margin: EdgeInsets.only(bottom: 0.0),
+                  decoration: BoxDecoration(
+                      color: ColorsArsenal.backgroundColorLight,
+                      border: Border(
+                          bottom:
+                              BorderSide(width: 1, color: Color(0x15000000)))),
                   currentAccountPicture: CircleAvatar(
                     radius: 50.0,
+                    backgroundColor: ColorsArsenal.backgroundColorLight,
                     backgroundImage: NetworkImage(
                         'https://avatars3.githubusercontent.com/u/33602013?s=400&u=67e085abb0eae2db24ce05c35da8252217c84e43&v=4'),
                   ),
-                  accountEmail: Text('gaaucocoelho@gmail.com'),
-                  accountName: Text('Gabriel Coelho'),
+                  accountEmail: Text(
+                    'gaaucocoelho@gmail.com',
+                    style: TextStyle(
+                        color: ColorsArsenal.textColorDark, fontSize: 16.0),
+                  ),
+                  accountName: Text(
+                    'Gabriel Coelho',
+                    style: TextStyle(
+                        color: ColorsArsenal.textColorDark,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.w700),
+                  ),
                 ),
               ),
               ListTile(
-                title: Text('Home'),
+                title: Text(
+                  'Home',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+                ),
                 onTap: () {
                   _pageController.jumpToPage(0);
                 },
               ),
               ListTile(
-                title: Text('Config'),
+                title: Text(
+                  'Config',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+                ),
                 onTap: () {
                   _pageController.jumpToPage(1);
                 },
               ),
               ListTile(
-                title: Text('About'),
+                title: Text(
+                  'About',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+                ),
                 onTap: () {
                   _pageController.jumpToPage(2);
                 },
               ),
               ListTile(
-                title: Text('Logout'),
+                title: Text(
+                  'Logout',
+                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.w600),
+                ),
                 onTap: () {
                   Navigator.pop(context);
                 },
