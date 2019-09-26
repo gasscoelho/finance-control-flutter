d4f313feabc4bc8434abd8b4f25b8e070867e5b9
BlaackRabbit <33602013+Potato@users.noreply.github.com>
2019-09-26 01:27:13 -0300

    Colors class created

---
+++ lib/util/colors_arsenal.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ColorsArsenal{
+  static const Color primaryColor = const Color(0XFF536290);
+  static const Color primaryColorDark = const Color(0XFF253862);
+  static const Color primaryColorLight = const Color(0XFF828fc1);
+  static const Color textColorDark = const Color(0XFF222222);
+  static const Color backgroundColorLight = const Color(0XFFFAFAFA);
+
+
+  static const Color white = const Color(0XFFFFFFFF);
+}
